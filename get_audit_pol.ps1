51d9f0b4b03891ff5d76fd09926cafa55d7e2b3e
Ken Davis <78566907+kd1882@users.noreply.github.com>
2023-04-12 10:43:46 -0400

    Update get_audit_pol.ps1

--- get_audit_pol.ps1
+++ get_audit_pol.ps1
@@ -1,17 +1,26 @@
 # Get Local Audit Policy settings
-Write-Host "Local Audit Policy settings" -ForegroundColor Yellow
 $auditCategories = @("System", "Logon/Logoff", "Object Access", "Privilege Use", "Detailed Tracking", "Policy Change", "Account Management", "Directory Service Access", "Account Logon")
 $localAuditPolicy = auditpol.exe /get /category:* | Select-String -Pattern '^(.+)\s+(Success|Failure|Success and Failure|No Auditing)'
-$localAuditPolicy | ForEach-Object {
+$localAuditResults = $localAuditPolicy | ForEach-Object {
     $auditCategory = $auditCategories[[int]($_.Line.Split()[0])]
     $auditSetting = $_.Line.Split()[1..($_.Line.Split().Length - 1)] -join ' '
     [PSCustomObject]@{
         Category = $auditCategory
         Setting  = $auditSetting
+        Type     = "Local Audit Policy"
     }
-} | Format-Table -AutoSize
+}
 
 # Get Advanced Audit Policy settings
-Write-Host "`nAdvanced Audit Policy settings" -ForegroundColor Yellow
 $advancedAuditPolicy = auditpol.exe /get /category:* /r
-$advancedAuditPolicy | ConvertFrom-Csv | Format-Table -AutoSize
+$advancedAuditResults = $advancedAuditPolicy | ConvertFrom-Csv | ForEach-Object {
+    [PSCustomObject]@{
+        Category = $_.Subcategory
+        Setting  = $_.InclusionSetting
+        Type     = "Advanced Audit Policy"
+    }
+}
+
+# Combine results and export to CSV
+$auditResults = $localAuditResults + $advancedAuditResults
+$auditResults | Export-Csv -Path "AuditRules.csv" -NoTypeInformation
